31efd910cc8421b9941bbbf958f75f01c4a641d4
sircatalin1983 <mcatalin1983@yahoo.com>
2019-03-01 09:05:32 +0000

    first commit

---
+++ devops-app/src/app/layout/tools/tool-detail/tool-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-detail',
+  templateUrl: './tool-detail.component.html',
+  styleUrls: ['./tool-detail.component.scss']
+})
+export class ToolDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
